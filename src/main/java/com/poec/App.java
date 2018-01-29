f3a41a95cb1124dc5c2fbb9e2e30561a7af5a70e
fabien barthelot <fabien.barthelot@gmail.com>
2018-01-29 11:31:24 +0100

    initialisation du projet java

---
+++ src/main/java/com/poec/App.java
@@ -0,0 +1,13 @@
+package com.poec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
