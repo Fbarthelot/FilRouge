b2a6e0d5782f89e7760dfaec3a8aa57602f922ba
poec3 <arraour.samy@yahoo.fr>
2018-01-30 16:00:25 +0100

    modif de app pour utiliser getFilm()

--- src/main/java/com/poec/App.java
+++ src/main/java/com/poec/App.java
@@ -1,13 +1,23 @@
 package com.poec;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        //afficher le résultat de la methode getFilm de la class RechercheFilm
 
+        // on crée l'objet RechercheFilm
+
+        RechercheFilm rFilm = new RechercheFilm();
+        // on appel la méthode getFilm et on attribut le résultat
+        Boolean filmExiste = new Boolean(false);
+
+
+        filmExiste = rFilm.getFilm("shining");
+
+        //on affiche le résultat
+
+        System.out.println(filmExiste);
     }
+
+
 }
+
